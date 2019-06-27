7d29df6e57855c7bbe47f8ecaceaf66f956b7017
Ubuntu <ubuntu@ip-172-31-39-199.ec2.internal>
2019-06-27 11:42:23 +0000

    updated js file

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,6 +1,5 @@
 var map;
 
-
 function initMap() {
             var options = {
                 center: {lat: 53.1424,lng: -7.6921},
@@ -8,48 +7,42 @@ function initMap() {
             };
             
             map = new google.maps.Map(document.getElementById('map'),options);
-          
-               
-            var input = document.getElementById('location-form');
-            var searchBox = new google.maps.places.SearchBox(input);
-            
-            map.addEventListener('bounds_changed', function(){
-                searchBox.setBounds(map.getBounds());
-            });
-            
-            var markers = [];
-            
-            searchBox.addListener('places_changed', function(){
-                var places = searchBox.getPlaces();
-                
-                if (places.length === 0)
-                return;
-                
-                markers.forEach(function(m){m.setMap(null); });
-                markers = [];
-                
-                var bounds = new google.maps.LatLngBounds();
-                
-                places.forEach (function (p){
-                    if (!p.geometry)
-                    return;
-                    
-                    markers.push(new google.maps.Marker({
-                        map: map,
-                        title: p.name,
-                        position: p.geometry.location
-                    }));
-                    
-                    if (p.geometry.viewport)
-                        bounds.union(p.geometry.viewport);
-                    else 
-                       bounds.extend(p.geometry.location);
-                });
-                
-                map.fitBounds(bounds);
-                
-            });
-        }     
+
+geocode();
+
+var location = 'Monaghan';
+
+function geocode() {
+    axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
+        params:{
+        address: location,
+        key: 'AIzaSyDj_Sw27lS1EGDp6aGNzfh9chD3YXd6A5c'
+        }
+    })
+    .then(function(responce){
+        // log full responce
+        console.log(responce);
         
+        // formatted address
+        
+        var formattedAddress = responce.data.results[0].formatted_address;
+        var formattedAddressOutput = `
             
-           
+            <ul class="list-group">
+            <li class="list-group-item>${formattedAddress}</li>
+            
+            </ul>
+        `;
+        
+        // output to app
+        
+        document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
+    })
+    
+    .catch(function(error){
+        console.log(error);
+    });
+    
+    
+}
+}
