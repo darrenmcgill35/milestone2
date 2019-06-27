908aa5f8c5f86fe31b2a9ae9817c5317892eae7f
Ubuntu <ubuntu@ip-172-31-39-199.ec2.internal>
2019-06-27 14:43:01 +0000

    Updated maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,32 @@
-var map;
+//var map;
 
-function initMap() {
-            var options = {
-                center: {lat: 53.1424,lng: -7.6921},
-                zoom: 6
-            };
+//function initMap() {
+            //var options = {
+              //  center: {lat: 53.1424,lng: -7.6921},
+              //  zoom: 6
+          //  };
             
-            map = new google.maps.Map(document.getElementById('map'),options);
+          //  map = new google.maps.Map(document.getElementById('map'),options);
+            
+            
+//geocode();
 
-geocode();
+// get location form
 
-var location = 'Monaghan';
+var locationForm = document.getElementById('location-form')
 
-function geocode() {
+//listen for submit
+
+locationForm.addEventListener('submit', geocode);
+
+
+
+function geocode(e) {
+    
+    e.preventDefault();
+    
+    var location = document.getElementById('location-input').value; 
+    
     axios.get('https://maps.googleapis.com/maps/api/geocode/json',{
         params:{
         address: location,
@@ -34,9 +48,36 @@ function geocode() {
             </ul>
         `;
         
+        //address components
+        
+        var addressComponents = responce.data.results[0].address_components;
+        var addressComponentsOutput = '<ul class="list-group">';
+        for(var i = 0; i < addressComponents.length;i++){
+            addressComponentsOutput += `
+                <li class="list-group-item><strong>${addressComponents[i].types[0]}</strong>:${addressComponents[i].long_name}</li>
+            `;
+        }
+        
+        addressComponentsOutput += '</ul>';
+        
+        // geometry
+        
+        var lat = responce.data.results[0].geometry.location.lat;
+        var lng = responce.data.results[0].geometry.location.lng;
+        var geometryOutput = `
+            
+            <ul class="list-group">
+            <li class="list-group-item><strong>Latitude</strong>${lat}</li>
+            <li class="list-group-item><strong>Longitude</strong>${lng}</li>
+            
+            </ul>
+        `;
+        
         // output to app
         
         document.getElementById('formatted-address').innerHTML = formattedAddressOutput;
+        document.getElementById('address-components').innerHTML = AddressComponentsOutput;
+        document.getElementById('geometry').innerHTML = geometryOutput;
     })
     
     .catch(function(error){
@@ -44,5 +85,4 @@ function geocode() {
     });
     
     
-}
 }
