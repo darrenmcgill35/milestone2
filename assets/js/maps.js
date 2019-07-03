6538a7442b2cc3a2bf727ba81a102d09e64b7076
Ubuntu <ubuntu@ip-172-31-39-199.ec2.internal>
2019-07-03 15:24:29 +0000

    Updated coords in maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -24,33 +24,205 @@ $(".newbridge").click(function () {
         center: {lat: 53.1796861, lng: -6.7968114},
         zoom: 16
     });
+    
+    //set the features
+    const features = [
+        {
+            position: new google.maps.LatLng(53.179693, -6.794662),
+            type: 'stadium'
+        },
+         {
+            position: new google.maps.LatLng(53.181101, -6.796377),
+            type: 'atm'
+        }, {
+            position: new google.maps.LatLng(53.179620, -6.799231),
+            type: 'atm'
+        },  {
+            position: new google.maps.LatLng(53.180595, -6.796134),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(53.178655, -6.794946),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(53.180883, -6.797312),
+            type: 'bars'
+        }, {
+            position: new google.maps.LatLng(53.179185, -6.799876),
+            type: 'bars'
+        }, {
+            position: new google.maps.LatLng(53.180529, -6.797896),
+            type: 'travel'
+        }
+    ];
+    // Create markers.
+    for (let i = 0; i < features.length; i++) {
+        var marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map
+        });
+    }
+    marker.setMap(map);
+    
+    $(".look-here").css( "visibility", "visible");
 });
+
+$(".derry").click(function () {
+    // do something for derry
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 54.992999, lng: -7.333914},
+        zoom: 16
+    });
+    
+    //set the features
+    const features = [
+        {
+            position: new google.maps.LatLng(54.993454, -7.333464),
+            type: 'stadium'
+        },
+         {
+            position: new google.maps.LatLng(54.991445, -7.331862),
+            type: 'atm'
+        },   {
+            position: new google.maps.LatLng(54.994271, -7.333177),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(54.994886, -7.335419),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(54.990836, -7.332836),
+            type: 'bars'
+        }, {
+            position: new google.maps.LatLng(54.994989, -7.327697),
+            type: 'bars'
+        }, {
+            position: new google.maps.LatLng(54.995395, -7.336028),
+            type: 'travel'
+        }
+    ];
+    // Create markers.
+    for (let i = 0; i < features.length; i++) {
+        var marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map
+        });
+    }
+    marker.setMap(map);
+    
+    $(".look-here").css( "visibility", "visible");
+});
+   
+$(".longford").click(function () {
+    // do something for Longford
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 53.738881,  lng: -7.803449},
+        zoom: 13
+    });
+    
+    //set the features
+    const features = [
+        {
+            position: new google.maps.LatLng(53.739858, -7.805316),
+            type: 'stadium'
+        },
+         {
+            position: new google.maps.LatLng(53.738764, -7.801979),
+            type: 'atm'
+        },   {
+            position: new google.maps.LatLng(53.741540, -7.804082),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(53.738196, -7.805219),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(53.728800, -7.801585),
+            type: 'bars'
+        },  {
+            position: new google.maps.LatLng(53.724305, -7.796032),
+            type: 'travel'
+        }
+    ];
+    // Create markers.
+    for (let i = 0; i < features.length; i++) {
+        var marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map
+        });
+    }
+    marker.setMap(map);
+    
+    $(".look-here").css( "visibility", "visible");
+});
+
+$(".carrick").click(function () {
+    // do something for Carrick
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 53.947879,   lng: -8.079018},
+        zoom: 15
+    });
+    
+    //set the features
+    const features = [
+        {
+            position: new google.maps.LatLng(53.948333, -8.076121),
+            type: 'stadium'
+        },
+         {
+            position: new google.maps.LatLng(53.943888, -8.070800),
+            type: 'atm'
+        },   {
+            position: new google.maps.LatLng(53.947058, -8.077366),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(53.945612, -8.081046),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(53.945887, -8.095492),
+            type: 'bars'
+        },  {
+            position: new google.maps.LatLng(53.945255, -8.095106),
+            type: 'bars'
+        }, {
+            position: new google.maps.LatLng(53.943306, -8.093277),
+            type: 'travel'
+        }
+    ];
+    // Create markers.
+    for (let i = 0; i < features.length; i++) {
+        var marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map
+        });
+    }
+    marker.setMap(map);
+    
+    $(".look-here").css( "visibility", "visible");
+});
+
+
 
 $(".clones").click(function () {
     // do something for clones
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: 54.1858495, lng: -7.2352085},
+        center: {lat: 54.183449,  lng: -7.234562},
         zoom: 15
     });
     //set the features
     const features = [
         {
-            position: new google.maps.LatLng(54.185869, -7.233718),
+            position: new google.maps.LatLng(54.185339, -7.234186),
             type: 'stadium'
         },
-        {
-            position: new google.maps.LatLng(54.181342, -7.234525),
-            type: 'atm'
-        }, {
+         {
             position: new google.maps.LatLng(54.179163, -7.231956),
             type: 'atm'
         }, {
             position: new google.maps.LatLng(54.180340, -7.228126),
             type: 'atm'
-        }, {
-            position: new google.maps.LatLng(54.181603, -7.225900),
-            type: 'atm'
-        }, {
+        },  {
             position: new google.maps.LatLng(54.186379, -7.234286),
             type: 'parking'
         }, {
@@ -64,6 +236,52 @@ $(".clones").click(function () {
             type: 'bars'
         }, {
             position: new google.maps.LatLng(54.181540, -7.232144),
+            type: 'travel'
+        }
+    ];
+    // Create markers.
+    for (let i = 0; i < features.length; i++) {
+        var marker = new google.maps.Marker({
+            position: features[i].position,
+            icon: icons[features[i].type].icon,
+            map: map
+        });
+    }
+    marker.setMap(map);
+    
+    $(".look-here").css( "visibility", "visible");
+});
+
+$(".newry").click(function () {
+    // do something for newry
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 54.168533,  lng:  -6.340492 },
+        zoom: 14
+    });
+    
+    //set the features
+    const features = [
+        {
+            position: new google.maps.LatLng(54.1614417, -6.334012),
+            type: 'stadium'
+        },
+         {
+            position: new google.maps.LatLng(54.169218, -6.339580),
+            type: 'atm'
+        },   {
+            position: new google.maps.LatLng(54.164111, -6.333293),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(54.168107, -6.341032),
+            type: 'parking'
+        }, {
+            position: new google.maps.LatLng(54.166022, -6.334181),
+            type: 'bars'
+        },  {
+            position: new google.maps.LatLng(54.173511, -6.342764),
+            type: 'bars'
+        }, {
+            position: new google.maps.LatLng(54.188803, -6.362567),
             type: 'travel'
         }
     ];
